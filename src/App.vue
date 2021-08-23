2bc3a80080783d5243ffb24032b3f4cff1f277d3
David Ackerman <enfipy@gmail.com>
2021-08-23 20:04:49 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <img alt="Donut logo" src="https://media.istockphoto.com/photos/donut-with-sprinkles-isolated-picture-id538335769?k=6&m=538335769&s=612x612&w=0&h=3tWMzxWCN99V21CcbUny2TQBsYklc1es3gARr4add7s=" />
+</template>
+
+<script setup>
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
